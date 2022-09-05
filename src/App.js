67565be2b00ffebfcf07d671d32d7dfadd381725
tuanminh2 <aroi671620@gmail.com>
2022-09-05 10:09:51 +0700

    pretty ok

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Calendar from './components/Calendar/Calendar';
 import StudentList from './components/StudentList/StudentList';
-import CustomDrawler from './components/CustomDrawler';
+
 import EditStudent from './components/EditStudent/EditStudent';
 import AddStudent from './components/AddStudent/AddStudent';
 
@@ -12,7 +12,7 @@ function App() {
 
   return (
     <>
-      <CustomDrawler></CustomDrawler>
+     
       <Routes>
         <Route path="/" element={<StudentList />} />
         <Route path="add" element={<AddStudent />} />
--- src/components/StudentList/StudentList.js
+++ src/components/StudentList/StudentList.js
@@ -1,39 +1,26 @@
 import { ContactsOutlined } from '@material-ui/icons';
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
+import Drawler from '../Drawler/Drawler';
 import './StudentList.scss';
 
 function StudentList() {
   const [studentList, setStudentList] = useState([]);
   const [studentListCp, setStudentListCp] = useState([]);
   const [name, setName] = useState('');
-  const [mail, setMail] = useState('');
+  const [email, setEmail] = useState('');
   const [gender, setGender] = useState('');
   const hdlSearchNameChange = (event) => {
     let nameSearch = event.target.value;
     setName(nameSearch);
-    if (nameSearch == '') setStudentListCp(studentList);
 
-    let rslist = studentList.filter(
-      (v, i) => v.name.trim().indexOf(nameSearch.trim()) !== -1
-    );
-    console.log(rslist);
-    console.log('----' + studentListCp);
-    setStudentListCp(rslist);
+    // let rslist = studentList.filter(
+    //   (v, i) => v.name.trim().indexOf(nameSearch.trim()) !== -1
+    // );
   };
   const hdlSearchMailChange = (event) => {
     let emailSearch = event.target.value;
-    setMail(emailSearch);
-    let rslist = [];
-
-    rslist = studentList.filter(
-      (v, i) => v.email.trim().indexOf(emailSearch.trim()) !== -1
-    );
-    if (studentListCp.length < studentList.length) {
-      rslist= rslist.filter((v, i) => studentListCp.includes(v));
-    }
-
-    setStudentListCp(rslist);
+    setEmail(emailSearch);
   };
   const hdlSearchGenderChange = (event) => {
     setGender(event.target.value);
@@ -59,87 +46,90 @@ function StudentList() {
       setStudentListCp(res);
     });
   }, []);
-  useEffect(() => {}, [name, mail, gender]);
+  useEffect(() => {}, [name, email, gender]);
   return (
-    <div id="studentListWrapper">
-      <h1 className="title">Student List</h1>
-      <div className="search-box">
-        <div className="search-item name">
-          <p>Search by Name</p>
-          <input
-            type="text"
-            value={name}
-            onChange={(e) => hdlSearchNameChange(e)}
-          />
-        </div>
-        <div className="search-item mail">
-          <p>Search by Email</p>
-          <input type="text" value={mail} onChange={hdlSearchMailChange} />
-        </div>
-        <div className="search-item gender">
-          <p>Gender</p>
-          <div className="gender-ratio-container">
-            <p>Male</p>
+    <div className="wrapper">
+      <Drawler></Drawler>
+      <div id="studentListWrapper">
+        <h1 className="title">Student List</h1>
+        <div className="search-box">
+          <div className="search-item name">
+            <p>Search by Name</p>
             <input
-              type="radio"
-              name="gender"
-              value="M"
-              onChange={hdlSearchGenderChange}
-            />
-            <p>FeMale</p>
-            <input
-              type="radio"
-              name="gender"
-              value="F"
-              onChange={hdlSearchGenderChange}
-            />
-            <p>All</p>
-            <input
-              type="radio"
-              name="gender"
-              value=""
-              onChange={hdlSearchGenderChange}
+              type="text"
+              value={name}
+              onChange={(e) => hdlSearchNameChange(e)}
             />
           </div>
+          <div className="search-item mail">
+            <p>Search by Email</p>
+            <input type="text" value={email} onChange={hdlSearchMailChange} />
+          </div>
+          <div className="search-item gender">
+            <p>Gender</p>
+            <div className="gender-ratio-container">
+              <p>Male</p>
+              <input
+                type="radio"
+                name="gender"
+                value="M"
+                onChange={hdlSearchGenderChange}
+              />
+              <p>FeMale</p>
+              <input
+                type="radio"
+                name="gender"
+                value="F"
+                onChange={hdlSearchGenderChange}
+              />
+              <p>All</p>
+              <input
+                type="radio"
+                name="gender"
+                value=""
+                onChange={hdlSearchGenderChange}
+              />
+            </div>
+          </div>
         </div>
+
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th>id</th>
+              <th>name</th>
+              <th>email</th>
+              <th>gender</th>
+              <th>team</th>
+              <th>action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {studentListCp &&
+              studentListCp.length > 0 &&
+              studentListCp.map((item, index) => {
+                return (
+                  <tr key={item.id}>
+                    <td>{item.id}</td>
+                    <td>{item.name}</td>
+                    <td>{item.email}</td>
+                    <td>{item.gender}</td>
+
+                    <td>{item.teams}</td>
+                    <td>
+                      <span
+                        className="del-student-btn"
+                        onClick={() => hdlDeleteStudent(item.id)}
+                      >
+                        <i className="fa fa-trash" aria-hidden="true"></i>
+                      </span>
+                    </td>
+                  </tr>
+                );
+              })}
+          </tbody>
+        </table>
       </div>
-
-      <table className="table table-striped">
-        <thead>
-          <tr>
-            <th>id</th>
-            <th>name</th>
-            <th>email</th>
-            <th>gender</th>
-            <th>team</th>
-            <th>action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {studentListCp &&
-            studentListCp.length > 0 &&
-            studentListCp.map((item, index) => {
-              return (
-                <tr key={item.id}>
-                  <td>{item.id}</td>
-                  <td>{item.name}</td>
-                  <td>{item.email}</td>
-                  <td>{item.gender}</td>
-
-                  <td>{item.teams}</td>
-                  <td>
-                    <span
-                      className="del-student-btn"
-                      onClick={() => hdlDeleteStudent(item.id)}
-                    >
-                      <i className="fa fa-trash" aria-hidden="true"></i>
-                    </span>
-                  </td>
-                </tr>
-              );
-            })}
-        </tbody>
-      </table>
     </div>
   );
 }
